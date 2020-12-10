3c42ea2002ab209d17469590b3d90e371a77198c
ahmad <ahmadf0311@gmail.com>
2020-12-10 19:11:14 +0100

    #26:started_with_icongraphy

--- etl/xml_importer/utils/sourceId.py
+++ etl/xml_importer/utils/sourceId.py
@@ -4,40 +4,8 @@ from etl.xml_importer.xpaths import namespace, paths
 
 class SourceID():
 
-    def __init__(self, sourceID):
-        self.root = sourceID
-        self.source = self._parse_source()
-        self.id = self._parse_id()
-        self.term = self._parse_term()
-
-    def _parse_source(self):
-        sourse = self.root.findall(paths["SourceID_Source_Path"], namespace)
-        if len(sourse) > 0:
-            source_name = sourse[0].attrib.get('{http://www.lido-schema.org}source')
-        else:
-            source_name = ''
-
-        #print(source_name)
-        return source_name
-
-    def _parse_id(self):
-        sourse = self.root.findall(paths["SourceID_Source_Path"], namespace)
-        if len(sourse) > 0:
-            id = get_id_by_prio(sourse)
-        else:
-            id = ''
-        #print(id)
-        return id
-
-    def _parse_term(self):
-        sourse = self.root.findall(paths["SourceID_Term_Path"], namespace)
-        if len(sourse) > 0:
-            term= sourse[0].text
-        else:
-            term = ''
-        #print(term)
-        return term
-
+    def __init__(self):
+        self
 #source string
 #id string
 #term string
