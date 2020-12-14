59eecebe4b42771b90149fc2096996655fac7ea0
Duong, Dinh-Phuong <dd.phuong.duong@gmail.com>
2020-12-14 20:00:01 +0100

    #9: handle dnb and getty display dynamically

--- app/src/app/shared/components/information/information.component.ts
+++ app/src/app/shared/components/information/information.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 import { Entity } from '../../models/models';
 import { SourceID } from '../../models/utils';
 
@@ -23,10 +23,36 @@ export class InformationComponent {
   @Input()
   sourceID: SourceID[];
 
+  sourceIDObject: any;
+
   constructor() {}
 
-  ngOnChanges() {
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.sourceID) {
+      this.handleSourceIDArray(changes.sourceID.currentValue);
+    }
     this.checkRequiredFields();
+  }
+
+  /**
+   * This Method converts sourceID Array to an object and checks
+   * if the element is a getty- or dnb-element and 'renames'
+   * the key to getty or dnb. These keys are checked inside
+   * the html-template
+   */
+  handleSourceIDArray(sID: SourceID[]) {
+    this.sourceIDObject = { ...sID };
+    for (let key in this.sourceIDObject) {
+      if(this.sourceIDObject[key].source.includes("vocab.getty.edu")) {
+        this.sourceIDObject['getty'] = this.sourceIDObject[key];
+        delete this.sourceIDObject[key];
+      } else {
+        if(this.sourceIDObject[key].source.includes("d-nb.info")) {
+          this.sourceIDObject['dnb'] = this.sourceIDObject[key];
+          delete this.sourceIDObject[key];
+        }
+      }
+    }
   }
 
   /**
