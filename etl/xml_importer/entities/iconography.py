ccdb231603e85eb36b4df50a2fb42a2667eb3a31
ahmad <ahmadf0311@gmail.com>
2020-12-14 21:35:39 +0100

    #26:class Icongraphy adaptet für SourceID, parse corrected, sanitize not completed

--- etl/xml_importer/entities/iconography.py
+++ etl/xml_importer/entities/iconography.py
@@ -1,4 +1,3 @@
-from etl.xml_importer.parseLido import get_id_by_prio
 from etl.xml_importer.utils.sourceId import SourceID
 from etl.xml_importer.xpaths import paths, namespace
 
@@ -8,26 +7,30 @@ class Iconography():
     def __init__(self, root):
         self.root = root
         self.id = self._parse_id()
-        self.type = 'iconography'
-        self.conceptID = self._parse_conceptID()
-        self.name = self._parse_name()
-        #print(self.name)
 
-    def _parse_id(self):
-        allIconographiesIDs = self.root.find(paths["Icongraphy_Id_Path"], namespace)
-        return allIconographiesIDs.text.split('/')[-1]
-
-    def _parse_conceptID(self):
-        allConceptIDs = []
-        for conceptID in self.root.findall(paths["Icongraphy_Id_Path"], namespace):
-            allConceptIDs.append(SourceID(conceptID))
-        return allConceptIDs
-
-    def _parse_name(self):
-        return self.root.find(paths["Icongraphy_Name_Path"], namespace).text
+    def _parse_id(self):##Todo:sanitize id
+        id = self.root.find(paths["Icongraphy_Id_Path"], namespace).text.split('/')[-1]
+        integer = id.find(')')
+        if integer > 0:
+            id = id[:integer+1]
+        return id
+    #11H(Francis)344(+3) -->11H(Francis)
+    #98B(Antiochus%20I)61 -->98B(Antiochus%20I)
+    #98B(Nero)52 -->98B(Nero)
+    #25H13 -->25H13
+    #61B2(...)11(+51) -->61B2(...)11(+51)
 
     def parse(self):
-        pass
+        self.entity_type= 'Iconography'
+        self.concepts = []
+        self.name = self.root.find(paths["Icongraphy_Name_Path"], namespace).text
+
+        for source_id in self.root.findall(paths["Icongraphy_Id_Path"], namespace):
+            concept = SourceID(source_id.text)
+            concept._parse_source()
+            concept._parse_term(self.root, "Icongraphy_Name_Path", "Icongraphy_Altname_Path")
+            self.concepts.append(concept)
+
 #id
 #entityType string
 #conceptID SourceID[]
