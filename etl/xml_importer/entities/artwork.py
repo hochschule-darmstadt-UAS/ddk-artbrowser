706e8ee7dd4f8f83671889ae36289e6bd58e5856
Yannick Mau <ymdev@xmau.de>
2020-12-27 23:26:39 +0100

    Add measurement parsing and start to add json encoding

--- etl/xml_importer/entities/artwork.py
+++ etl/xml_importer/entities/artwork.py
@@ -8,6 +8,7 @@ from etl.xml_importer.entities.type import Type
 from etl.xml_importer.entities.location import Location
 from etl.xml_importer.utils.recordLegal import RecordLegal
 from etl.xml_importer.utils.resource import Resource
+from etl.xml_importer.utils.measurement import Measurement
 from etl.xml_importer.xpaths import namespace
 
 artists = dict()
@@ -17,12 +18,15 @@ materials = dict()
 iconographys = dict()
 types = dict()
 
+
 class Artwork():
 
     def __init__(self, lido):
         self.lido = lido
+        self.entity_type = "artwork"
+
         self.id = self._parse_id()
-        self.name = self._parse_name()
+        self.label = self._parse_label()
         self.inscriptions = self._parse_inscription()
         self.types = self._parse_types()
         self.genres = self._parse_genres()
@@ -30,14 +34,13 @@ class Artwork():
         self.artists = self._parse_artists()
         self.iconographies = self._parse_iconographies()
         self.materials = self._parse_materials()
-        #self.measurements = _parse_measurements()
-        self.recordLegal = self._parse_record_legal()
+        self.measurements = self._parse_measurements()
+        self.recordLegal = self._parse_recordLegal()
         self.resources = self._parse_resource()
 
-    #TODO: diese Attribute haben eine niedrige Prio, daher erstmal nicht weiter beachten
+    # TODO: diese Attribute haben eine niedrige Prio, daher erstmal nicht weiter beachten
     # artwork.altName string[] 2
     # artwork.inscriptions string[] 2
-
 
     def _parse_id(self):
         # TODO: Was soll ich hier nur lido herausnehmen?: DE - Mb112 - lido - t3 - 000230
@@ -46,9 +49,9 @@ class Artwork():
         id = id.replace("/", "-").replace(",", "-").replace("lido-", "").replace("obj-", "")
         return id
 
-    def _parse_name(self):
-        name = self.lido.find(paths["Artwork_Name_Path"], namespace).text
-        return name
+    def _parse_label(self):  # TODO: Format DE-Mb112-00000000001
+        label = self.lido.find(paths["Artwork_Name_Path"], namespace)
+        return label.text
 
     def _parse_inscription(self):
         inscriptions = []
@@ -80,8 +83,8 @@ class Artwork():
             if genre_.id not in genres:
                 genre_.parse()
             genres[genre_.id] = genre_
-        #print(genreIDs)
-        #print(genres)
+        # print(genreIDs)
+        # print(genres)
         return genreIDs
 
     def _parse_location(self):
@@ -129,29 +132,28 @@ class Artwork():
                 materials[material_.id] = material_
         return materialIDs
 
-
     ####################################################################################
 
-    def _parse_measurements(lido):
+    def _parse_measurements(self):
         measurements = []
-        #TODO: alle measurements fuer das uebergebene lido heraussuchen
-        measurement = lido.measurement #Typ Measurement
-        measurements.append(measurement)
+        # TODO: alle measurements fuer das uebergebene lido heraussuchen
+
+        measurments_sets = self.lido.findall(paths["Artwork_Measurements_Path"], namespace)
+        for measurment_root in measurments_sets:
+            measurements.append(Measurement(measurment_root))
 
         return measurements
 
-        #measurments_sets = self.root.findall(paths["Artwork_Measurements_Path"], namespace)
-        #print(len(measurments_sets))
-        #if (len(measurments_sets) > 0):
-          #  self.artwork["measurements"].append(Measurment(measurments_sets).getmeasurment())
-        #else:
-           # pass
+    def _parse_recordLegal(self):
+        recordLegal_root = self.lido.find(paths["Artwork_RecordLegal_Path"], namespace)
+        if recordLegal_root is not None:
+            return RecordLegal(recordLegal_root)
+        else:
+            return None
 
-    def _parse_record_legal(self):
-        record_legal_root = self.lido.find(paths["Artwork_RecordLegal_Path"], namespace)
-        record_legal = RecordLegal(record_legal_root)
 
-        return record_legal
+    # for recordLegal in recordLegal_List:
+    # self.artwork["recordLegal"] = RecordLegal(recordLegal).get_recordLegal()
 
     def _parse_resource(self):
         allresources = []
@@ -160,21 +162,31 @@ class Artwork():
             allresources.append(Resource(resource))
 
         return allresources
-        #resourceLegal_List = self.root.findall(paths["Artwork_ResourceLegal_Path"], namespace)
-        #if (len(resourceLegal_List) > 0):
-           # self.artwork["resourceLegal"] = Resource(resourceLegal_List).getresourceLegal()
+        # resourceLegal_List = self.root.findall(paths["Artwork_ResourceLegal_Path"], namespace)
+        # if (len(resourceLegal_List) > 0):
+        # self.artwork["resourceLegal"] = Resource(resourceLegal_List).getresourceLegal()
 
     def _parse_altName(lido):
-        #TODO: altName fuer das uebergebene lido heraussuchen
+        # TODO: altName fuer das uebergebene lido heraussuchen
         altName = lido.altName
 
         return altName
 
-        #altenames_List = self.root.findall(paths["Artwork_Altename_Path"], namespace)
-        #altenames = []
-        #if len(altenames_List) > 0:
+        # altenames_List = self.root.findall(paths["Artwork_Altename_Path"], namespace)
+        # altenames = []
+        # if len(altenames_List) > 0:
         #    for altename in  altenames_List:
         #        altenames.append(altename.text)
-        #else:
+        # else:
         #    pass
-        #self.artwork["altName"] = altenames
+        # self.artwork["altName"] = altenames
+
+    def __json_repr__(self):
+        return {
+            "id": self.id,
+            "entityType": self.entity_type,
+            "label": self.label,
+            "measurements": self.measurements,
+            "recordLegal": self.recordLegal,
+            "resources": self.resources,
+        }
