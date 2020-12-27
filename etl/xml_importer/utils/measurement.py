1ffecc0b5ee5f4be0877f89556ebe643352f06f5
Yannick Mau <ymdev@xmau.de>
2020-12-27 23:19:04 +0100

    Implement measurement parsing

--- etl/xml_importer/utils/measurement.py
+++ etl/xml_importer/utils/measurement.py
@@ -1,64 +1,23 @@
-#TODO: noch ueberarbeiten
+from xpaths import paths, namespace
 
-class Measurment():
-    def __init__(self, displayName,
-                 #measurementType, unit, value,
-                 #extend, shape, format,
-                 #qualifier,
-                 #measurments_sets
-                 ):
-        #self.root = measurments_sets
-        self.displayName = displayName
-        #self.parse()
+
+class Measurement:
+    def __init__(self, root):
+        self.root = root
+        self.displayName = ""
+        self.parse()
 
     def parse(self):
-       # self.measurment["displaySize"] = self.root[0].findall(paths["Artwork_Mesurments_DisplaySize_Path"], namespace)[0].text
-       # self.measurment["type"] = self.root[0].findall(paths["Artwork_Mesurments_Type_Path"], namespace)[0].text
-       # self.measurment["unit"] = self.root[0].findall(paths["Artwork_Mesurments_Unit_Path"], namespace)[0].text
-       # self.measurment["value"] = self.root[0].findall(paths["Artwork_Mesurments_Value_Path"], namespace)[0].text
+        self.displayName = self._parse_displayName()
 
-        measurmentsets = []
-        for measurments_set in self.root:
-        #    measurmentset = measurments_set.findall('lido:displayObjectMeasurements', namespace)
-          #  if len(measurmentset) > 0:
-         #       measurmentsets.append(measurmentset[0].text)
-         #   else:
-      #          pass
+    def _parse_displayName(self):
+        displayName = self.root.find(paths["Measurement_DisplayName_Path"], namespace)
+        if displayName != None:
+            return displayName.text
 
-          #  shape = measurments_set.findall(paths["Artwork_Mesurments_Shape_Path"], namespace)
-         #   if len(shape) > 0:
-          #      self.measurment["shape"] = shape[0].text
-          #  else:
-          #      pass
+        return ""
 
-          #  format = measurments_set.findall(paths["Artwork_Mesurments_Format_Path"], namespace)
-            if len(format) > 0:
-                self.measurment["format"] = format[0].text
-            else:
-                pass
-
-          #  qualifier = measurments_set.findall(paths["Artwork_Mesurments_Qualifier_Path"], namespace)
-          #  if len(qualifier) > 0:
-          #      self.measurment["qualifier"] = qualifier[0].text
-           # else:
-           #     pass
-
-        self.measurment["displaySize"] = measurmentsets[0]
-
-        if len(measurmentsets) > 1:
-            self.measurment["displayName"] = measurmentsets[1]
-        else:
-            pass
-
-    def getmeasurment(self):
-        return self.measurment
-
-
-#displayName string
-#measurementType string 2
-#unit string 2
-#value string 2
-#extend string 2
-#shape string 2
-#format string 2
-#qualifier string 2
+    def __json_repr__(self):
+        return {
+            "displayName": self.displayName,
+        }
