7e8e6503de496837deb168d9b5e819f591a1cbd5
Yannick Mau <ymdev@xmau.de>
2020-12-27 23:19:19 +0100

    Implement Rights parsing

--- etl/xml_importer/utils/rights.py
+++ etl/xml_importer/utils/rights.py
@@ -1,7 +1,30 @@
-class Rights():
+from etl.xml_importer.utils.sourceId import SourceID
+from etl.xml_importer.xpaths import paths, namespace
 
-    def __init__(self, root):
+
+class Rights:
+
+    def __init__(self, root, ):
         self.root = root
+        self.types = self._parse_types()
+        self.holders = self._parse_holders()
 
-#rightsType SourceID
-#rightsHolder SourceID
+    def _parse_types(self):
+        rights_type = self.root.find(paths['Rights_Type_Path'], namespace)
+        if rights_type is not None:
+            return SourceID(rights_type)
+        else:
+            return None
+
+    def _parse_holders(self):
+        rights_holder = self.root.find(paths['Rights_Holder_Path'], namespace)
+        if rights_holder is not None:
+            return SourceID(rights_holder, term_path='lido:legalBodyName/lido:appellationValue')
+        else:
+            return None
+
+    def __json_repr__(self):
+        return {
+            "rightsType": self.types,
+            "rightsHolder": self.holders,
+        }
