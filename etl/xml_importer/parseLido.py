1bed9657608ef69d2b7c9005ea1d330073bf57cd
Yannick Mau <ymdev@xmau.de>
2020-12-29 16:15:56 +0100

    Adjust sanitize_id function and add sanitize function for terms and labels

--- etl/xml_importer/parseLido.py
+++ etl/xml_importer/parseLido.py
@@ -1,9 +1,30 @@
-def sanitize(id): #TODO: ID-Format definieren, da auch mal Leerzeichen vorkommen können
+import re
+
+
+def sanitize_id(id): #TODO: ID-Format definieren, da auch mal Leerzeichen vorkommen können
     spec_char = ["(", ")"] #Special character list, you can add new char's
     for char in spec_char:
-        id.replace(char, "-")
+        id = id.replace(char, "-")
+
+    id = id.strip()
 
     return id
+
+
+def sanitize(text: str):
+    """
+    This function removes linebreaks, carriage returns, duplicated spaces and all leading
+    and trailing spaces from the passed string and returns the sanitized one. This function should be used
+    for fields which contain longer strings like 'label' or 'term'.
+    """
+    # remove newline and carriage return
+    sanitized_text = text.replace('\n', ' ').replace('\r', '')
+    # remove duplicated spaces and remove leading and trailing spaces
+    sanitized_text = re.sub(' +', ' ', sanitized_text)
+    sanitized_text = sanitized_text.strip()
+
+    return sanitized_text
+
 
 def get_id_by_prio(all_ids):
     id = ""
@@ -19,7 +40,8 @@ def get_id_by_prio(all_ids):
             id = "ddk-" + current_type_id.text.split('/')[-1]
             break
 
-    return sanitize(id)
+    return sanitize_id(id)
+
 
 def sanitize_location(location): ##TODO: Method besser definieren
     spec_char = [" — ", " / ", " "]
