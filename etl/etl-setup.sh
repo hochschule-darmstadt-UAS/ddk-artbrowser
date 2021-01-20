96e4201e96c4d15f108ee54577f3409eabab6f1d
Yannick Mau <ymdev@xmau.de>
2021-01-20 16:05:03 +0100

    Add input directory creation and hint

--- etl/etl-setup.sh
+++ etl/etl-setup.sh
@@ -10,3 +10,7 @@ source venv/bin/activate
 
 # install pip requirements into the virtual python environment
 pip install -r requirements.txt
+
+# create the input directory for the xml files
+mkdir -p ./input
+echo "Put your lido xml files into the directory ./input before running etl.sh"
