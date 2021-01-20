446a4f99d5f25a6fe5f15283378febc928480249
Yannick Mau <ymdev@xmau.de>
2021-01-20 15:27:45 +0100

    Remove typo

--- etl/etl-setup.sh
+++ etl/etl-setup.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y python3-virtualenv
 # create virtual python environment in directory 'venv'
 virtualenv venv
 
-# activate virtual environment fpr current shell session
+# activate virtual environment for current shell session
 source venv/bin/activate
 
 # install pip requirements into the virtual python environment
