52be731425f4974c3edc5b8dd005ea54bff1b1b0
MadelineWall <74000328+MadelineWall@users.noreply.github.com>
2021-01-30 12:21:47 +0100

    Issue: https://github.com/hochschule-darmstadt-UAS/ddk-artbrowser/issues/86
    Logging etl process to a file

--- etl/etl-setup.sh
+++ etl/etl-setup.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+mkdir -p /log_etl.log
+LOG_FILE=/log_etl-setup.log
+
 # this must be installed by a root/sudo user
 #sudo apt-get install -y python3-virtualenv
 
 # create virtual python environment in directory 'venv'
+echo $(date -u) "INFO: Create virtual python environment in directory 'venv'" | ${LOG_FILE}
 virtualenv venv
 
 # activate virtual environment for current shell session
+echo $(date -u) "INFO: Activate virtual environment for current shell session" | ${LOG_FILE}
 source venv/bin/activate
 
 # install pip requirements into the virtual python environment
+echo $(date -u) "INFO: Install pip requirements into the virtual python environment" | ${LOG_FILE}
 pip install -r requirements.txt
 
 # create the input directory for the xml files
 mkdir -p ./input
-echo "Put your lido xml files into the directory ./input before running etl.sh"
+echo $(date -u) "INFO: Put your lido xml files into the directory ./input before running etl.sh" | ${LOG_FILE}
--- etl/etl.sh
+++ etl/etl.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
+mkdir -p /log_etl.log
+LOG_FILE=/log_etl.log
+
+if [ -f ./venv ] ;then
+
+echo $(date -u) "INFO: Use the virtual python environment: source venv/bin/activate" | ${LOG_FILE}
 source venv/bin/activate
 
+echo $(date -u) "INFO: Handle paths of input xml-files and output json files" | ${LOG_FILE}
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/.."
 
 mkdir -p ./output
 
+echo $(date -u) "INFO: Run the xml-importer with theses input xml-files and store the json files" | ${LOG_FILE}
 python xml_importer/main.py ./input ./output
 
+echo $(date -u) "INFO: Run the elasticsearch_uploader.py script with the generated json files." | ${LOG_FILE}
 python upload_to_elasticsearch/elasticsearch_uploader.py ./output
+
+else
+echo $(date -u) "ERROR: folder venv not available. Please run first etl-setup.sh" | ${LOG_FILE}
+exit 1;
+fi
+
+
+
+
+
